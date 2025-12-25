009a41f352c925dc7effcd884e260033113d49a7
Utkarsh-Shaning <utkarsh1605shaning@gmail.com>
2025-12-26 04:05:49 +0530

    added SpringSecurityConfig,Keycloak 26.4.7,Configured gateway-service as an OAuth2 resource server and updated pom.xml,spring-boot-starter-oauth2-resource-server,spring-boot-starter-security

---
+++ GATEWAY SERVICE/gateway-service/gateway-service/src/main/java/com/utkarshhh/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.utkarshhh.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeExchange(exchange -> exchange
+                        .pathMatchers("/actuator/**").permitAll()
+                        .pathMatchers("/eureka/**").permitAll()
+                        .anyExchange().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> {})
+                );
+
+        return http.build();
+    }
+}
